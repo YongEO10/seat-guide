6709df0aab15e79cabe2e52c0255090a9a571a74
YongEO10 <jeaniwijaya62@gmail.com>
2026-01-21 11:21:51 +0700

    Create data.js

---
+++ data.js
@@ -0,0 +1,19 @@
+<script>
+const guests = {
+  "INV001": {
+    name: "Bapak Andi Wijaya",
+    table: "Meja 5",
+    area: "VIP"
+  },
+  "INV002": {
+    name: "Ibu Maria",
+    table: "Meja 12",
+    area: "Reguler"
+  },
+  "INV003": {
+    name: "Kel. Budi Santoso",
+    table: "Meja 3",
+    area: "Keluarga"
+  }
+};
+</script>
